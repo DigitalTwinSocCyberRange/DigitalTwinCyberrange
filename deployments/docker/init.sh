fb79a73cf3fa9305c58b7071fb213232c70e2f43
lenaglas <lena.glas@web.de>
2021-02-22 10:49:29 +0000

    changes in api functions

--- deployments/docker/init.sh
+++ deployments/docker/init.sh
@@ -1,7 +1,8 @@
 chown root $(find conf/filebeat/ conf/filebeat-es/ -name "*.yml")
 pkill screen
-echo -n "Enter the Hostname or IP Address where your elasticsearch will be deployed:"
-read ip
+#echo -n "Enter the Hostname or IP Address where your elasticsearch will be deployed:"
+ip=$(ip addr show enp1s0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+
 echo "HOST_IP=$ip" > .env
 
 docker-compose up -d
--- deployments/docker/start_docker_api.sh
+++ deployments/docker/start_docker_api.sh
@@ -1,11 +1,17 @@
-
+cd /root/DigitalTwin-Cyberrange/deployments/docker
 #restart docker containers
 pkill screen
 docker-compose stop 
 docker-compose up -d
 
 #restart api
-cd ./../../src/pyrest
+cd ./../../src
+screen -dmSL main bash deactivate_directives.sh -Logfile
+cd pyrest
 screen -dmSL main python api.py -Logfile
+
+cd ./../../../frontendCyberrange
+screen -dmSL main npm run serve -Logfile
+
 
 
