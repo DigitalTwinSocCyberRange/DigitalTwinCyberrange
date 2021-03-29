7bf5552e87415f100fc37ce943dba69b5d780a05
lenaglas <lena.glas@web.de>
2021-03-29 11:31:06 +0000

    pip

--- setup_python.sh
+++ setup_python.sh
@@ -1,4 +1,5 @@
 apt-get install python3 &&
+sudo apt-get install python3-distutils &&
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py &&
 python3 get-pip.py &&
 pip3 install flask flask-cors screen
