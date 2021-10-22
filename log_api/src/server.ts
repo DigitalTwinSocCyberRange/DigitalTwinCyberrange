78933b7cf4d390d31a66d63bc0198f10e53d78e1
antumin <m.vielberth@gmail.com>
2021-10-22 14:06:32 +0200

    Add write log changes to stream

--- log_api/src/server.ts
+++ log_api/src/server.ts
@@ -3,17 +3,17 @@ import fs from 'fs';
 import readLastLines from "read-last-lines";
 // run().catch(err => console.log(err));
 
-export class ExpressServer{
+export class ExpressServer {
   app = express();
-  logfile = './index.html';
+  logfiles = ['./index.html', './server.js'];
 
-  constructor(){
-    
+  constructor() {
+
   }
 
-  public start(): void{
+  public start(): void {
     console.log('starting...')
-    this.app.get('/events', async (req: any, res: any)=>{
+    this.app.get('/events', async (req: any, res: any) => {
       console.log('Got /events');
       res.set({
         'Cache-Control': 'no-cache',
@@ -21,35 +21,44 @@ export class ExpressServer{
         'Connection': 'keep-alive'
       });
       res.flushHeaders();
-  
+
       // Tell the client to retry every 10 seconds if connectivity is lost
       res.write('retry: 10000\n\n');
       let count = 0;
-  
-      this.watchFile(this.logfile, (lastline: string) =>{
+
+      this.watchFiles(this.logfiles, (lastline: string) => {
         console.log(lastline);
+        this.writeLog(res, lastline);
       });
     });
 
-    const index = fs.readFileSync('./index.html', 'utf8'); 
+    const index = fs.readFileSync('./index.html', 'utf8');
     this.app.get('/', (req: any, res: any) => res.send(index));
 
     this.app.listen(3000);
     console.log('Listening on port 3000');
   }
 
-  private async readLastLine(filename: string): Promise<string>{
-      return await readLastLines.read(filename, 1);
+  private async readLastLine(filename: string): Promise<string> {
+    return await readLastLines.read(filename, 1);
   }
 
-  private writeLog(res: any, log: string){
+  private writeLog(res: any, log: string) {
     res.write(`data: ${log}\n\n`);
   }
 
-  private watchFile(filename: string, lastline: (lastline: string) => void){
-    fs.watch(filename, async (event: any, filename: any) => {  
+  private watchFile(filename: string, lastline: (lastline: string) => void) {
+    fs.watch(filename, async (event: any, filename: any) => {
       console.log(`${filename} file changed`);
       lastline(await this.readLastLine(filename));
+    });
+  }
+
+  private watchFiles(filenames: string[], lastlines: (lastline: string) => void) {
+    filenames.forEach((filename: string) => {
+      this.watchFile(filename, (lastline) => {
+        lastlines(lastline);
+      });
     });
   }
 }
