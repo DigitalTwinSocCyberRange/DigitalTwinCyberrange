ab2da8bd35ea7a747c30a64830eca7863dffed32
lenaglas <lena.glas@web.de>
2022-01-17 07:43:40 +0000

    Update api stop

--- deployments/docker/docker_stop.sh
+++ deployments/docker/docker_stop.sh
@@ -1,7 +1,4 @@
-cd ..
-bash deactivate_directives.sh
-screen -X -S frontend quit
-cd ../deployments/docker
+
 sudo docker-compose stop 
 sudo docker container rm elasticsearch 
 sudo docker volume rm docker_es-data 
