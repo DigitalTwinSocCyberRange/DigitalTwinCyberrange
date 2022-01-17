10a85863532f637f36546309824ff7a82b9a6533
lenaglas <lena.glas@web.de>
2022-01-17 07:34:27 +0000

    Update VM api restart

--- deployments/docker/vm_restart.sh
+++ deployments/docker/vm_restart.sh
@@ -2,5 +2,8 @@ docker-compose stop
 sudo docker container rm elasticsearch
 sudo docker volume rm docker_es-data
 docker-compose up -d
+cd ./../../src
+cd pyrest
+screen -dmSL main bash deactivate_directives.sh -Logfile
 
 
